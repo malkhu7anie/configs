bf40ad6e4e7f25620d470f2259b50d7db1a78901
Mohammed Alkhuzanie <malkhu7anie@gmail.com>
2022-02-06 19:20:58 +0200

    Add emacs.d

---
+++ .emacs.d/elpa/colorless-themes-0.2/colorless-themes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "colorless-themes" "0.2" "A macro to generate mostly colorless themes" '((emacs "24.1")) :commit "8e539097185704444a5dd5545924de92c11ba767" :authors '(("Thomas Letan" . "contact@thomasletan.fr")) :maintainer '("Thomas Letan" . "contact@thomasletan.fr") :keywords '("faces themes" "faces") :url "https://git.sr.ht/~lthms/colorless-themes.el")
---
+++ .emacs.d/elpa/with-editor-3.1.1/with-editor-pkg.el
@@ -0,0 +1,12 @@
+(define-package "with-editor" "3.1.1" "Use the Emacsclient as $EDITOR"
+  '((emacs "24.4"))
+  :commit "a4e720b12a0946a271a274bbe0b45ae07f83520b" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/with-editor")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
